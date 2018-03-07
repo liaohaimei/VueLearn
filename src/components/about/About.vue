78e55e7e811f31300e31753286fc9b4ed01b9f6f
liaohaimei <sz_liaohaimei@163.com>
2018-03-07 16:53:24 +0800

    1

---
+++ src/components/about/About.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="row mb-5">
+            <div class="col-4">
+                <ul class="list-group">
+                    <router-link tag="li" class="nav-link" :to="{name:'historyLink'}">
+                        <a class="list-group-item list-group-item-action list-group-item-light">历史订单</a>
+                    </router-link>
+                    <router-link tag="li" class="nav-link" :to="{name:'contactLink'}">
+                        <a class="list-group-item list-group-item-action list-group-item-light">联系我们</a>
+                    </router-link>
+                    <router-link tag="li" class="nav-link" :to="{name:'orderingGuideLink'}">
+                        <a class="list-group-item list-group-item-action list-group-item-light">点餐文档</a>
+                    </router-link>
+                    <router-link tag="li" class="nav-link" :to="{name:'deliveryLink'}">
+                        <a class="list-group-item list-group-item-action list-group-item-light">快递信息</a>
+                    </router-link>
+                </ul>
+            </div>
+            <div class="col-8 mt-2">
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App.vue'
+import Home from './components/Home'
+import Menu from './components/Menu'
+import Admin from './components/Admin'
+import About from './components/about/About'
+import Login from './components/Login'
+import Register from './components/Register'
+
+// 二级路由
+import Contact from './components/about/Contact'
+import Delivery from './components/about/Delivery'
+import History from './components/about/History'
+import OrderingGuide from './components/about/OrderingGuide'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {path:'/',name:'homeLink',component:Home},
+  {path:'/menu',name:'menuLink',component:Menu},
+  {path:'/admin',name:'adminLink',component:Admin},
+  {path:'/about',name:'aboutLink',redirect:'/contact',component:About,children:[
+    {path:'/contact',name:'contactLink',component:Contact},
+    {path:'/delivery',name:'deliveryLink',component:Delivery},
+    {path:'/history',name:'historyLink',component:History},
+    {path:'/orderingGuide',name:'orderingGuideLink',component:OrderingGuide}
+  ]},
+  {path:'/login',name:'loginLink',component:Login},
+  {path:'/register',name:'registerLink',component:Register},
+  {path:'*',redirect:'/'}
+]
+
+const router = new VueRouter({
+  routes,
+  mode:'history'
+})
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
